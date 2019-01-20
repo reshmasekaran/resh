27df7e94b7f50f85046d872583f8fba8ec6e0bc7
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-01-20 21:22:38 +0530

    set1 2.c

---
+++ set1 2.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n%2==0)
+    {
+        printf("even");
+    }
+else if(n%2!=0)
+{
+    printf("odd");
+}
+//else
+//{
+    //printf("zero");
+//}
+   return 0;
+}
