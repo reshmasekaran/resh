435baf78b9a292d5a2a22720d875116cdd45965a
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-01-29 19:35:20 +0530

    Create Set9.1.c

---
+++ Set9.1.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+ int a,b,dif;
+ scanf("%d%d",&a,&b);
+ dif=a-b;
+ printf ("%d",dif);
+
+    return 0;
+}
