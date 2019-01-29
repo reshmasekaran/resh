b9ed5c31e02b94dfef3e4293c63bbb7b7a509d2b
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-01-29 05:06:08 -0800

    Create set1.4q.c

---
+++ set1.4q.c
@@ -0,0 +1,13 @@
+include <stdio.h>
+
+int main()
+{
+    char ch;
+    scanf("%d",&ch);
+    if(ch>=a && ch<=z)||(ch<=Z && ch>=A)
+    printf("alphabet");
+    else
+    printf("not alphabet");
+
+    return 0;
+}
