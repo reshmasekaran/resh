fe762b2aa3b45c009898b0ca857fdb4164493d19
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-01-30 21:56:11 +0530

    Create Se1 5th.c

---
+++ Se1 5th.c
@@ -0,0 +1,17 @@
+void main()
+{
+int a,b,c;
+if((a>b)&&(a>c))
+{
+printf("%d a is greter",a);
+}
+else if((b>a)&&(b>c))
+{
+printf("%d b is greater",b );
+}
+else
+{
+printf("%d c is greter",c);
+}
+getch();
+}
