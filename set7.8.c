82ff35d6868934cfb479c70fa5439541db9e6634
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-02-10 15:53:19 +0530

    Create set7.8.c

---
+++ set7.8.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+	
+	int main()
+	{
+	   int num;
+	   scanf("%d",&num);
+	   if(num%7==0)
+	   {
+	       printf("yes");
+	   }
+	   else
+	   {
+	       printf("no");
+	   }
+	    return 0;
+	}
