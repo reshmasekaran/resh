a7bda7c3e9bc0810e8d7832c7c58d78b5558fa21
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-02-10 16:18:28 +0530

    Update set8.2.c

--- set8.2.c
+++ set8.2.c
@@ -2,14 +2,14 @@
 	
 	int main()
 	{
-	    char a[10],i,j,b[10],count,c=0;
+	    char s[10],i,j,k[10],count,c=0;
 	    scanf("%s",a);
-	    for(i=0;a[i]!='\0';i++);
+	    for(i=0;s[i]!='\0';i++);
 	    count=i;
 	    j=0;
 	    for(i=0;i<count;i++)
 	    {
-	        if((a[i]=='a')||(a[i]=='e')||(a[i]=='i')||(a[i]=='o')||(a[i]=='u')||(a[i]=='A')||(a[i]=='E')||(a[i]=='I')||(a[i]=='O')||(a[i]=='U'))
+	        if((s[i]=='a')||(s[i]=='e')||(s[i]=='i')||(s[i]=='o')||(s[i]=='u')||(s[i]=='A')||(s[i]=='E')||(s[i]=='I')||(s[i]=='O')||(s[i]=='U'))
 	        {
 	            c++;
 	        }
