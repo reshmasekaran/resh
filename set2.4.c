8fab9b4cdbeaede035077f3aed372cfe8ec460d8
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-02-10 08:30:00 +0530

    Create set2.4.c

---
+++ set2.4.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+	#include<conio.h>
+	void main()
+	{
+	int a,b,i;
+	clrscr();
+	scanf("%d%d",&a,&b);
+	for(i=a;i<=b;i++)
+	{
+	if(i%2==1)
+	{
+	printf(" %d",i);
+	}
+	}
+	getch();
+	}
+Set2.5
+#include<stdio.h>
+	#include<conio.h>
+	void main()
+	{
+	int a,b,i;
+	clrscr();
+	scanf("%d%d",&a,&b);
+	for(i=a;i<=b;i++)
+	{
+	if(i%2==0)
+	{
+	printf(" %d",i);
+	}
+	}
+	getch();
+	}
