4b57b81b06e5f844c88a5e8d0ecca68cdd7a2b75
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-02-11 02:20:54 -0800

    Create set10.2.c

---
+++ set10.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,n,sum=0,i=0;
+    scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a);
+}
+for(i=0;i<=a;i++)
+    {
+        sum=sum+i;
+    }
+    printf("%d",sum);
+}
