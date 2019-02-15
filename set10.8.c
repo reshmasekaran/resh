2477d27a4508fe2bcaba6ea8743eb8cf4e896f6a
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-02-15 03:03:11 -0800

    Create set10.8.c

---
+++ set10.8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+    int b[50],i,n;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+        if(b[i]!=i)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+}
