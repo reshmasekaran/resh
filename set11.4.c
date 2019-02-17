4d3096cb8782f9cdf4d2bb0a09983aa5d1c3e7df
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-02-17 16:18:21 +0530

    Update set11.4.c

--- set11.4.c
+++ set11.4.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    int n,k,res=1,i;
-    scanf("%d %d",&n,&k);
-    for(i=0;i<k;i++)
+    int n,n1,res=1,i;
+    scanf("%d %d",&n,&n1);
+    for(i=0;i<n1;i++)
     {
-        res=res*n;
+        r=r*n;
     }
     printf("%d",res);
 	return 0;
