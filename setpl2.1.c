30785753781a5282d7bdf2e1a3451a7d0a825bc8
reshmasekaran <46868251+reshmasekaran@users.noreply.github.com>
2019-02-20 02:04:53 -0800

    Create setpl2.1.c

---
+++ setpl2.1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[50],i=0;
+    scanf("%s",&a);
+    if((a[i]=='m')||(a[i]=='t')||(a[i]=='w')||(a[i]=='f'))
+    {
+        printf("no");
+    }
+    else
+    {
+        printf("yes");
+    }
+
+   
+    return 0;
+}
